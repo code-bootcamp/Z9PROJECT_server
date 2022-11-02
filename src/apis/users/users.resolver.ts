e0f9ebed7b4ff16264d6a20009c28fdf2a480744
Clark19 <clarkgable1@gmail.com>
2022-11-02 11:27:02 +0900

    Issue 20 update user (#46)
    * INIT: Nest.JS Initializing (#3)
    
    * Feature : 유저 Entity 생성(#17)
    
    유저 테이블용 Entity Class 생성
    
    closed #17
    
    * Delete app.controller.ts
    
    * FEATURE: Add Library
    
    Add minio, uuid, graphql-upload
    Add following types to dev dependencies
    @types/graphql-upload, @types/uuid, @types/minio
    
    closed #37
    
    * TEST: CI: test production environment setup(#43)
    
    add Dockerfile.test
    add docker-compose.test.yaml
    update .env for production setup
    update app.module.ts for database connection and graphql
    
    closed #43
    
    * Feature : API-createUser 구현(#18)
    
    회원 가입용 createUser() API 구현
    
    closed #18
    
    * Feature : API-updateUser 구현(#20)
    
    회원 정보 수정용 updateUser() API 구현
    
    closed #20
    
    Co-authored-by: BrianHong <ushiii111798@users.noreply.github.com>
    Co-authored-by: Brian Hong <ushiii111798@gmail.com>

--- src/apis/users/users.resolver.ts
+++ src/apis/users/users.resolver.ts
@@ -1,8 +1,8 @@
 import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
-
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/createUser.input';
+import { UpdateUserInput } from './dto/updateUser.input';
 
 @Resolver()
 export class UsersResolver {
@@ -16,5 +16,16 @@ export class UsersResolver {
   @Mutation(() => User)
   async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
     return this.usersService.create(createUserInput);
+  }
+  
+  @Mutation(() => User)
+  async updateUser(
+    @Args('userId') userId: string,
+    @Args('updateUserInput') updateUserInput: UpdateUserInput,
+  ) {
+    return this.usersService.update({
+      userId,
+      updateUserInput,
+    });
   }
 }
