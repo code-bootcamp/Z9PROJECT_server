f9967f4dd879f8a8d78c31b36e348a2363ee0f09
HWANGKYOJIN <108543999+HWANGKYOJIN@users.noreply.github.com>
2022-11-07 17:40:28 +0900

    FEATURE: API-createProduct 기능 구현(#66) createProduct API 생성 closed #66

--- src/apis/product/product.resolver.ts
+++ src/apis/product/product.resolver.ts
@@ -1,1 +1,37 @@
-export class ProductResolver {}
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateProductInput } from './dto/createProduct.input';
+import { UpdateProductInput } from './dto/updateProduct.input';
+import { Product } from './entities/product.entity';
+import { ProductService } from './product.service';
+
+@Resolver()
+export class ProductResolver {
+  constructor(
+    private readonly productService: ProductService, //
+  ) {}
+
+  @Query(() => Product)
+  fetchProduct(@Args('productId') productId: string) {
+    return this.productService.findOne({ productId });
+  }
+  @Query(() => [Product])
+  fetchProducts() {
+    return this.productService.findAll();
+  }
+
+  @Mutation(() => Product)
+  createProduct(
+    @Args('createProductInput') createProductInput: CreateProductInput,
+  ) {
+    return this.productService.create({ createProductInput });
+  }
+
+  @Mutation(() => Product)
+  async updateProduct(
+    @Args('productId') productId: string,
+    @Args('updateProductInput') updateProductInput: UpdateProductInput,
+  ) {
+    await this.productService.checkSoldout({ productId });
+    return this.productService.update({ productId, updateProductInput });
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CacheModule, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import 'dotenv/config';
+import { ProductModule } from './apis/product/product.module';
 
 @Module({
   imports: [
+    ProductModule,
     ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
@@ -21,11 +23,9 @@ import { AppController } from './app.controller';
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
       entities: [__dirname + '/apis/**/*.entity.*'],
-      synchronize: true,
       logging: true,
+      synchronize: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [],
 })
 export class AppModule {}
