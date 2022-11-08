cef8aa847e442c90123cc2abd62ef498c033a99d
Brian Hong <ushiii111798@gmail.com>
2022-11-08 11:36:52 +0900

    HOTFIX: update image.module

--- src/apis/images/image.module.ts
+++ src/apis/images/image.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { ImageResolver } from './image.resolver';
 import { ImageService } from './image.service';
 import { Image } from './entities/image.entity';
+import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Image])],
-  providers: [ImageResolver, ImageService],
+  providers: [ImageResolver, ImageService, UsersService],
 })
 export class ImageModule {}
