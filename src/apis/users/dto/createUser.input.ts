c23757479a5b758f8eb3b3952f19e6b19a8161e7
Brian Hong <ushiii111798@gmail.com>
2022-11-09 22:55:17 +0900

    FIX: FEATURE: 이미지 업로드 로직 수정 -> 유저 및 상품 엔티티 업데이트
    상품 테이블에 직접 이미지 URL을 저장하도록 수정
    유저 테이블에 직접 이미지 URL을 저장하도록 수정
    이미지 업로드시 URL 정보만 테이블에 저장하도록 수정
    
    closed #96

--- src/apis/users/dto/createUser.input.ts
+++ src/apis/users/dto/createUser.input.ts
@@ -1,5 +1,5 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
-import { FileUpload, GraphQLUpload } from 'graphql-upload';
+import { stringify } from 'uuid';
 import { SNS_TYPE_ENUM, USER_TYPE_ENUM } from '../entities/user.entity';
 
 @InputType()
@@ -10,58 +10,58 @@ export class CreateUserInput {
   @Field(() => String)
   password: string;
 
+  @Field(() => USER_TYPE_ENUM, { defaultValue: USER_TYPE_ENUM.COMMON_USER })
+  userType: USER_TYPE_ENUM;
+
   @Field(() => String)
   nickname: string;
 
   @Field(() => String)
   phoneNumber: string;
 
-  @Field(() => String, { nullable: true })
-  zipcode?: string;
+  @Field(() => String)
+  zipcode: string;
+
+  @Field(() => String)
+  address: string;
+
+  @Field(() => stringify)
+  addressDetail: string;
 
   @Field(() => String, { nullable: true })
-  address?: string;
+  profileImg: string;
 
   @Field(() => String, { nullable: true })
-  addressDetail?: string;
+  creatorAuthImg: string;
+
+  @Field(() => Boolean, { defaultValue: false })
+  isAuthedCreator: boolean;
 
   /** YouTube ChannelName or Instagram Name */
   @Field(() => String, { nullable: true })
-  snsName?: string;
+  snsName: string;
 
   @Field(() => SNS_TYPE_ENUM)
-  snsChannel?: SNS_TYPE_ENUM;
-
-  @Field(() => Boolean, { defaultValue: false })
-  isAuthedCreator?: boolean;
+  snsChannel: SNS_TYPE_ENUM;
 
   @Field(() => Int, { defaultValue: 0, nullable: true })
-  followerNumber?: number;
-
-  @Field(() => GraphQLUpload, { nullable: true })
-  userProfileImg?: FileUpload;
-
-  @Field(() => GraphQLUpload, { nullable: true })
-  creatorAuthImg?: FileUpload;
+  followerNumber: number;
 
   @Field(() => String, { nullable: true })
-  mainContents?: string;
+  mainContents: string;
 
   @Field(() => String, { nullable: true })
-  introduce?: string;
+  introduce: string;
 
   @Field(() => String, { nullable: true })
-  bank?: string;
+  bank: string;
 
   @Field(() => String, { nullable: true })
-  account?: string;
+  account: string;
 
   @Field(() => String, { nullable: true })
-  accountName?: string;
+  accountName: string;
 
-  @Field(() => Int, { nullable: true })
-  point?: number;
-
-  @Field(() => USER_TYPE_ENUM, { defaultValue: USER_TYPE_ENUM.COMMON_USER })
-  userType: USER_TYPE_ENUM;
+  @Field(() => Int, { defaultValue: 0 })
+  point: number;
 }
