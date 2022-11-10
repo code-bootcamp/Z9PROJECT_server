0f032d28b3e75963357492ddf6b79b6f4125317a
Brian Hong <ushiii111798@gmail.com>
2022-11-10 13:48:27 +0900

    FEATURE: Add likeProduct API
    This API can be used for liking product and unliking product
    Status will automatically updated
    
    closed #100

--- src/apis/productLike/productLike.resolver.ts
+++ src/apis/productLike/productLike.resolver.ts
@@ -1,31 +1,36 @@
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, CONTEXT, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { GqlAuthAccessGuard } from 'src/common/auth/gql-auth.guard';
+import { IContext } from 'src/common/types/context';
 import { ProductLikeService } from './productLike.service';
 
 @Resolver()
 export class ProductLikeResolver {
   constructor(private readonly productLikeService: ProductLikeService) {}
 
+  @UseGuards(GqlAuthAccessGuard)
   @Mutation(() => Boolean)
-  async likeProduct() {
-    return true;
-  }
-
-  @Mutation(() => Boolean)
-  async unlikeProduct() {
-    return true;
-  }
-
-  // TODO: For Future Use
-  // Pending Development
-  @Mutation(() => Boolean)
-  async likeProductComment() {
-    return true;
+  async likeProduct(
+    @Args('productId') productId: number,
+    @Context() ctx: IContext,
+  ) {
+    return this.productLikeService.likeProduct({
+      productId,
+      userId: ctx.req.user.id,
+    });
   }
 
   // TODO: For Future Use
   // Pending Development
-  @Mutation(() => Boolean)
-  async unlikeProductComment() {
-    return true;
-  }
+  // @Mutation(() => Boolean)
+  // async likeProductComment() {
+  //   return true;
+  // }
+
+  // TODO: For Future Use
+  // Pending Development
+  // @Mutation(() => Boolean)
+  // async unlikeProductComment() {
+  //   return true;
+  // }
 }
