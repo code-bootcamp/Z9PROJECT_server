2c0020d7bc5af419360da74116805b5c04cd1cb2
Brian Hong <ushiii111798@gmail.com>
2022-11-11 16:25:06 +0900

    FIX: update findAllLikes logic
    changed return value to product instead of productLike
    
    closed #134

--- src/apis/productLike/productLike.module.ts
+++ src/apis/productLike/productLike.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { ProductLike } from './entities/productLike.entity';
 import { ProductLikeService } from './productLike.service';
 import { ProductLikeResolver } from './productLike.resolver';
+import { ProductService } from '../product/product.service';
+import { Product } from '../product/entities/product.entity';
 
 // TODO: Fix Imports
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductLike])],
-  providers: [ProductLikeService, ProductLikeResolver],
+  imports: [TypeOrmModule.forFeature([ProductLike, Product])],
+  providers: [ProductLikeService, ProductLikeResolver, ProductService],
 })
 export class ProductLikeModule {}
