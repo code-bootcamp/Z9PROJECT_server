360423f98ad836ba36528685565672084ff7fe96
Brian Hong <ushiii111798@gmail.com>
2022-11-11 11:11:25 +0900

    FEATURE: Add optional search apis related to product
    add fetchProductsByCreator
    add fetchProductsByStatus
    
    closed #102

--- src/apis/product/product.module.ts
+++ src/apis/product/product.module.ts
@@ -1,3 +1,4 @@
+import { UsersService } from './../users/users.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductDetail } from '../productDetail/entities/productDetail.entity';
@@ -5,14 +6,21 @@ import { ProductDetailService } from '../productDetail/productDetail.service';
 import { Product } from './entities/product.entity';
 import { ProductResolver } from './product.resolver';
 import { ProductService } from './product.service';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Product, //
       ProductDetail,
+      User,
     ]),
   ],
-  providers: [ProductResolver, ProductService, ProductDetailService],
+  providers: [
+    ProductResolver,
+    ProductService,
+    ProductDetailService,
+    UsersService,
+  ],
 })
 export class ProductModule {}
--- src/apis/product/product.resolver.ts
+++ src/apis/product/product.resolver.ts
@@ -3,7 +3,11 @@ import { CreateProductDetailInput } from '../productDetail/dto/createProductDeta
 import { CreateProductInput } from './dto/createProduct.input';
 import { UpdateProductInput } from './dto/updateProduct.input';
 import { Product } from './entities/product.entity';
-import { ProductService } from './product.service';
+import {
+  ProductService,
+  PRODUCT_INCLUDE_OPTION,
+  PRODUCT_SEARCH_TYPE,
+} from './product.service';
 
 @Resolver()
 export class ProductResolver {
@@ -16,9 +20,26 @@ export class ProductResolver {
     return this.productService.findOne({ productId });
   }
 
-  @Query(() => [Product], { description: 'fetching mutiple product' })
+  @Query(() => [Product], { description: 'fetching multiple product' })
   fetchProducts() {
     return this.productService.findAll();
+  }
+
+  @Query(() => [Product], {
+    description: 'fetching multiple product by creator nickname',
+  })
+  fetchProductsByCreator(@Args('nickname') nickname: string) {
+    return this.productService.findProductByCreator({ name: nickname });
+  }
+
+  @Query(() => [Product], {
+    description: 'fetching multiple product by status',
+  })
+  fetchProductsByStatus(
+    @Args('type') type: PRODUCT_SEARCH_TYPE,
+    @Args('option') option: PRODUCT_INCLUDE_OPTION,
+  ) {
+    return this.productService.findProductByStatus({ type, option });
   }
 
   @Query(() => Number, { description: 'count product by userId' })
