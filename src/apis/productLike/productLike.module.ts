2f7d2bb88828a113704d6557c84bfd8c013499d3
Brian Hong <ushiii111798@gmail.com>
2022-11-11 17:04:19 +0900

    HOTFIX: update module import on productLike

--- src/apis/productLike/productLike.module.ts
+++ src/apis/productLike/productLike.module.ts
@@ -5,10 +5,22 @@ import { ProductLikeService } from './productLike.service';
 import { ProductLikeResolver } from './productLike.resolver';
 import { ProductService } from '../product/product.service';
 import { Product } from '../product/entities/product.entity';
+import { ProductDetail } from '../productDetail/entities/productDetail.entity';
+import { ProductDetailService } from '../productDetail/productDetail.service';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/entities/user.entity';
 
 // TODO: Fix Imports
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductLike, Product])],
-  providers: [ProductLikeService, ProductLikeResolver, ProductService],
+  imports: [
+    TypeOrmModule.forFeature([ProductLike, Product, ProductDetail, User]),
+  ],
+  providers: [
+    ProductLikeResolver,
+    ProductLikeService,
+    ProductService,
+    ProductDetailService,
+    UsersService,
+  ],
 })
 export class ProductLikeModule {}
