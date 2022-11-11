9c53324d2b41d1f77fd9d9ae336e7e1e83e56e27
Brian Hong <ushiii111798@gmail.com>
2022-11-11 12:12:28 +0900

    HOTFIX: update array input to JSON.stringify

--- src/apis/product/product.service.ts
+++ src/apis/product/product.service.ts
@@ -170,7 +170,7 @@ export class ProductService {
     console.log(product);
     const savedProduct: Product = await this.productRepository.save({
       ...product,
-      images: images,
+      images: JSON.stringify(images),
       discountRate: calcDiscountRate,
     });
 
@@ -206,7 +206,7 @@ export class ProductService {
 
     const newProduct: Product = await this.productRepository.save({
       ...originProduct,
-      images: images,
+      images: JSON.stringify(images),
       discountRate: calcDiscountRate,
       ...product,
     });
