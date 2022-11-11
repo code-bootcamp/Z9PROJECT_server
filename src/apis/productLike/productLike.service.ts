3b2c5eacea2e1b2be3d47c54e19aed86a7f5c125
Brian Hong <ushiii111798@gmail.com>
2022-11-11 21:25:13 +0900

    FIX: likeProduct undefined check

--- src/apis/productLike/productLike.service.ts
+++ src/apis/productLike/productLike.service.ts
@@ -23,7 +23,8 @@ export class ProductLikeService {
       .where('productLike.productId = :productId', { productId })
       .andWhere('productLike.userId = :userId', { userId })
       .getOne();
-    if (checkLike) {
+    console.log(checkLike);
+    if (checkLike !== undefined) {
       // 이미 좋아요 상태인지 확인 (deteledAt 값이 null인지 확인)
       if (checkLike.deletedAt) {
         // 좋아요 상태가 아닌 경우 좋아요로 변경
