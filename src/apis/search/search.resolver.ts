589e2995e2544f94046c3a4561d870a97e8826c2
Clark19 <clarkgable1@gmail.com>
2022-11-16 01:35:17 +0900

    FEATURE: 검색(Elasticsearch, Redist) 구현 (#106)
    상품 List 페이지용 검색 api 구현 : searchProducts() API (상품명, 공구진행 크리에이터 닉네임 으로 검색)
    크레에이터 검색 페이지용 검색 api 구현 : searchCreators API (닉네임, InstaName/youtube채널명 으로 검색)
    
    < 구현 상세 >
    elasticsearch 로 구현(multiple input, mutiple table&column),
    레디스로 검색결과 캐싱 구현
    각 검색 필드에 ngram 적용(template.json 사용)

---
+++ src/apis/search/search.resolver.ts
@@ -0,0 +1,74 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CREATOR_IDX, PRODUCT_IDX, SearchService } from './search.service';
+import { SearchOutput } from './dto/search.output';
+import { SearchProductOutput } from './dto/search.product.output';
+import { SearchCreatorOutput } from './dto/search.creator.output';
+
+@Resolver()
+export class SearchResolver {
+  constructor(private readonly searchService: SearchService) {}
+
+  @Query(() => [SearchCreatorOutput], {
+    nullable: true,
+    description:
+      'searching creators In Creator Page by nickname or UtubeChannelName/InstaName',
+  })
+  async searchCreators(
+    @Args({
+      name: 'word',
+      type: () => String,
+      nullable: true,
+      defaultValue: '',
+    })
+    word: string,
+  ) {
+    //LOGGING
+    console.log('검색 Creator api: ');
+
+    const cacheRst = await this.searchService.getCache(word, CREATOR_IDX);
+    if (cacheRst) {
+      return cacheRst;
+    }
+
+    const creatorSearchRst: SearchCreatorOutput[] =
+      await this.searchService.searchCreators(word);
+
+    if (creatorSearchRst) {
+      await this.searchService.setCache(word, creatorSearchRst, CREATOR_IDX);
+    }
+
+    return creatorSearchRst;
+  }
+
+  @Query(() => [SearchProductOutput], {
+    nullable: true,
+    description:
+      'searching products In Product List Page by product name or creator nickname',
+  })
+  async searchProducts(
+    @Args({
+      name: 'word',
+      type: () => String,
+      nullable: true,
+      defaultValue: '',
+    })
+    word: string,
+  ) {
+    //LOGGING
+    console.log('검색 Product api: ');
+
+    const cacheRst = await this.searchService.getCache(word, PRODUCT_IDX);
+    if (cacheRst) {
+      return cacheRst;
+    }
+
+    const productSearchRst: SearchProductOutput[] =
+      await this.searchService.searchProducts(word);
+
+    if (productSearchRst) {
+      await this.searchService.setCache(word, productSearchRst, PRODUCT_IDX);
+    }
+
+    return productSearchRst;
+  }
+}
